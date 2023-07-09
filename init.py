fb9a60fa394a6f61bdbfe11974185d9f710a0d9e
Atakan Kanburoglu <88630930+AtakanKanburoglu947@users.noreply.github.com>
2023-07-09 11:35:58 +0300

    Update init.py

--- init.py
+++ init.py
@@ -4,7 +4,6 @@ from env import create_env_file
 from packages import install_packages
 from config import create_config_folder
 from app import create_app
-
 def create_nodejs_api(project_name):
     try:
         new_directory = os.path.join(os.getcwd(), project_name)
@@ -16,13 +15,8 @@ def create_nodejs_api(project_name):
         create_config_folder()
         create_env_file()
         create_app()
-
         print("Node.js API created successfully.")
     except Exception as exception:
         print("An error occurred:", str(exception))
-
-
-
-
 project_name = "nodejs_api"
 create_nodejs_api(project_name)
